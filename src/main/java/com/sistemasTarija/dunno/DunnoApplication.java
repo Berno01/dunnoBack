4d69e4b6c4f7bdeed5e2cbb164572788761e7a3d
Berno01 <ruedabernardo2@gmail.com>
2025-12-07 03:20:33 -0400

    Fix: Zona Horaria

--- src/main/java/com/sistemasTarija/dunno/DunnoApplication.java
+++ src/main/java/com/sistemasTarija/dunno/DunnoApplication.java
@@ -1,7 +1,10 @@
 package com.sistemasTarija.dunno;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class DunnoApplication {
@@ -9,5 +12,8 @@ public class DunnoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DunnoApplication.class, args);
 	}
-
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/La_Paz"));
+    }
 }
